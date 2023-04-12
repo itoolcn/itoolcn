a405ace40c02d82bac125444f76735a78d7e1cc1
itoolcn <130525637+itoolcn@users.noreply.github.com>
2023-04-12 11:53:17 +0800

    222

--- static/script/dist/index/public/common.js
+++ static/script/dist/index/public/common.js
@@ -1,21 +1,21 @@
 define("public/common", ["jquery"], function(o, n, t) {
     o("jquery"),
     function() {
-        var o = navigator.userAgent.toLowerCase();
-        if (-1 < o.indexOf("bot") || -1 < o.indexOf("spider") || o.match(/msie [5678]/))
-            return;
-        navigator.geolocation && navigator.geolocation.getCurrentPosition(function(o) {
-            var n = o.coords.longitude
-              , t = o.coords.latitude
-              , e = "";
-            navigator.connection && (e = navigator.connection.effectiveType),
-            fetch("https://report.ipchaxun.com/?lng=" + n + "&lat=" + t + "&network=" + e + "&source=" + location.host, {
-                method: "GET"
-            }).then(function(o) {
-                o.json().then(function(o) {})
-            }).catch(function(o) {
-                console.log("error: " + o.toString())
-            })
-        })
+        // var o = navigator.userAgent.toLowerCase();
+        // if (-1 < o.indexOf("bot") || -1 < o.indexOf("spider") || o.match(/msie [5678]/))
+        //     return;
+        // navigator.geolocation && navigator.geolocation.getCurrentPosition(function(o) {
+        //     var n = o.coords.longitude
+        //       , t = o.coords.latitude
+        //       , e = "";
+        //     navigator.connection && (e = navigator.connection.effectiveType),
+        //     fetch("https://report.ipchaxun.com/?lng=" + n + "&lat=" + t + "&network=" + e + "&source=" + location.host, {
+        //         method: "GET"
+        //     }).then(function(o) {
+        //         o.json().then(function(o) {})
+        //     }).catch(function(o) {
+        //         console.log("error: " + o.toString())
+        //     })
+        // })
     }()
 });
